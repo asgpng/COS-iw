6494909841206af610baa3f7b2b6502a29c2dbee
Adam Gallagher <asgpng@gmail.com>
2013-06-19 16:06:04 -0400

    partially working form submission

--- iw.py
+++ iw.py
@@ -1,5 +1,6 @@
 import os
 import urllib
+import datetime
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -13,15 +14,15 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'])
 
 class SignupForm(ndb.Model):
-    studentName = ndb.StringProperty()
-    classYear = ndb.IntegerProperty()
+    student_name = ndb.StringProperty()
+    class_year = ndb.IntegerProperty()
     coursework = ndb.StringProperty(choices=set(["397", "398", "497", "498", "AB JIW", "AB Senior Thesis", "BSE Senior Thesis"]))
     title = ndb.StringProperty()
     description = ndb.StringProperty()
-    facultySignature = ndb.BooleanProperty()
-    facutlyName = ndb.StringProperty()
-    facutlyDepartment = ndb.StringProperty()
-    studentSignature = ndb.BooleanProperty()
+    faculty_signature = ndb.BooleanProperty()
+    faculty_name = ndb.StringProperty()
+    faculty_department = ndb.StringProperty()
+    student_signature = ndb.BooleanProperty()
     submitted = ndb.BooleanProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
@@ -60,11 +61,43 @@ class SignupFormPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('signupform.html')
         self.response.write(template.render(template_values))
 
-#    def post(self):
-        
+    def post(self):
 
+        sf = SignupForm(student_name=self.request.get('student_name'),
+                        class_year = int(self.request.get('class_year')),
+                        coursework = self.request.get('coursework'),
+                        title = self.request.get('titles'),
+                        description = self.request.get('description'),
+                        faculty_signature = bool(self.request.get('faculty_signature')),
+                        faculty_name = self.request.get('faculty_name'),
+                        faculty_department = self.request.get('faculty_department'),
+                        student_signature = bool(self.request.get('student_signature')))
+
+        sf.put()
+
+        self.redirect('/forms/signupform/view')
+
+class SignupFormPageView(webapp2.RequestHandler):
+        
+    def get(self):
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+        template = JINJA_ENVIRONMENT.get_template('signup_form_view.html')
+        self.response.write(template.render(template_values))
+
+    
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/forms/signupform', SignupFormPage),
+    ('/forms/signupform/view', SignupFormPageView),
 ], debug=True)
