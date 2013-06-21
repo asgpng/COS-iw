735c0b3236217bced5fcb14824eef7ce4dcb17b7
Adam Gallagher <asgpng@gmail.com>
2013-06-21 14:44:56 -0400

    query implementation

--- iw.py
+++ iw.py
@@ -23,7 +23,7 @@ def getLoginStatus(uri):
     return (url, url_linktext)
 
 class SignupForm(ndb.Model):
-    form_type = ndb.StringProperty()
+    form_type = ndb.StringProperty(default="signup")
     student_netID = ndb.StringProperty()
     student_name = ndb.StringProperty()
     class_year = ndb.IntegerProperty()
@@ -37,9 +37,10 @@ class SignupForm(ndb.Model):
     student_signature = ndb.BooleanProperty()
     submitted = ndb.BooleanProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
+    # consider adding properties = ndb.PickleProperty() which is a list of the properties of each form
 
 class CheckpointForm(ndb.Model):
-    form_type = ndb.StringProperty("checkpoint")
+    form_type = ndb.StringProperty(default="checkpoint")
     student_netID = ndb.StringProperty()
     student_name = ndb.StringProperty()
     topic_title = ndb.StringProperty()
@@ -53,7 +54,7 @@ class CheckpointForm(ndb.Model):
     comments = ndb.StringProperty()
 
 class FebruaryForm(ndb.Model):
-    form_type = ndb.StringProperty("february")
+    form_type = ndb.StringProperty(default="february")
     student_netID = ndb.StringProperty()
     student_name = ndb.StringProperty()
     title = ndb.StringProperty()
@@ -71,7 +72,7 @@ class FebruaryForm(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 class SecondReaderForm(ndb.Model):
-    form_type = ndb.StringProperty("second")
+    form_type = ndb.StringProperty(default="second_reader")
     student_netID = ndb.StringProperty()
     student_name = ndb.StringProperty()
     class_year = ndb.IntegerProperty()
@@ -82,7 +83,6 @@ class SecondReaderForm(ndb.Model):
     sr_department = ndb.StringProperty()
     sr_agreement = ndb.BooleanProperty()
     sr_signature = ndb.StringProperty()
-    
 
 class MainPage(webapp2.RequestHandler):
 
@@ -115,7 +115,6 @@ class SignupFormPage(webapp2.RequestHandler):
                         advisor_name = self.request.get('advisor_name'),
                         advisor_department = self.request.get('advisor_department'),
                         student_signature = bool(self.request.get('student_signature')),
-                        form_type = "signup",
                         student_netID = "test"
                         )
 
@@ -146,8 +145,7 @@ class CheckPointFormPage(webapp2.RequestHandler):
                              meet_more_often = bool(self.request.get('meet_more_often')),
                              student_progress = int(self.request.get('student_progress')),
                              comments = self.request.get('comments'),
-                             form_type = "checkpoint",
-                             student_netID = "test"
+                             student_netid = "test"
                              )
         cpf.put()
 
@@ -175,7 +173,6 @@ class SecondReaderFormPage(webapp2.RequestHandler):
                                sr_department = self.request.get('sr_department'),
                                sr_agreement =bool(self.request.get('sr_agreement')),
                                sr_signature = self.request.get('sr_signature'),
-                               form_type = "second_reader",
                                student_netID = "test"
                                )
 
@@ -207,7 +204,6 @@ class FebruaryFormPage(webapp2.RequestHandler):
                           advisor_more_meetings = bool(self.request.get('advisor_more_meetings')),
                           student_progress_eval = int(self.request.get('student_progress_eval')),
                           advisor_comments = self.request.get('advisor_comments'),
-                          form_type = "february",
                           student_netID = "test"
                       )
         ff.put()
@@ -225,13 +221,13 @@ class FormView(webapp2.RequestHandler):
             query = SignupForm.query(SignupForm.student_netID==student_netID)
             form = query.fetch(1)[0] # this might be buggy
         elif form_type == 'february':
-            query = FebruaryForm.query(SignupForm.student_netID==student_netID)
+            query = FebruaryForm.query(FebruaryForm.student_netID==student_netID)
             form = query.fetch(1)[0]
         elif form_type == 'checkpoint':
-            query = SignupForm.query(SignupForm.student_netID==student_netID)
+            query = CheckpointForm.query(CheckpointForm.student_netID==student_netID)
             form = query.fetch(1)[0]
-        elif form_type == 'second_reader': # form_type == 'second_reader':
-            query = FebruaryForm.query(SignupForm.student_netID==student_netID)
+        else:# form_type == 'second_reader':
+            query = SecondReaderForm.query(SecondReaderForm.student_netID==student_netID)
             form = query.fetch(1)[0]
 
         template_values = {
@@ -253,13 +249,76 @@ class FormQuery(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
     def post(self):
-        args = ['student_name', 'student_netID', 'advisor_name', 'advisor_netID', 'form_type']
+        # form_type is required, so get it separately:
+        form_type = self.request.get('form_type')
+        # additional arguments:
+        args = ['student_name', 'student_netID', 'advisor_name', 'advisor_netID']
         query_params = {}
         for arg in args:
             arg_get = self.request.get(arg)
             if arg_get != 'Null':
                 query_params[arg] = arg_get
-        self.redirect('/forms/view?' + urllib.urlencode(query_params))
+
+        # Is there a better way of doing this?
+        if form_type == 'signup':
+            query = SignupForm.query()
+            if 'student_name' in query_params:
+                query.filter(SignupForm.student_name==query_params['student_name'])
+            if 'student_netID' in query_params:
+                query.filter(SignupForm.student_netID==query_params['student_netID'])
+            if 'advisor_name' in query_params:
+                query.filter(SignupForm.advisor_name==query_params['advisor_name'])
+            if 'advisor_netID' in query_params:
+                query.filter(SignupForm.advisor_netID==query_params['advisor_netID'])
+            forms = query.fetch(20) # this might be buggy
+        elif form_type == 'february':
+            query = FebruaryForm.query()
+            if 'student_name' in query_params:
+                query.filter(FebruaryForm.student_name==query_params['student_name'])
+            if 'student_netID' in query_params:
+                query.filter(FebruaryForm.student_netID==query_params['student_netID'])
+            if 'advisor_name' in query_params:
+                query.filter(FebruaryForm.advisor_name==query_params['advisor_name'])
+            if 'advisor_netID' in query_params:
+                query.filter(FebruaryForm.advisor_netID==query_params['advisor_netID'])
+            forms = query.fetch(20)
+        elif form_type == 'checkpoint':
+            query = CheckpointForm.query()
+            if 'student_name' in query_params:
+                query.filter(CheckpointForm.student_name==query_params['student_name'])
+            if 'student_netID' in query_params:
+                query.filter(CheckpointForm.student_netID==query_params['student_netID'])
+            if 'advisor_name' in query_params:
+                query.filter(CheckpointForm.advisor_name==query_params['advisor_name'])
+            if 'advisor_netID' in query_params:
+                query.filter(CheckpointForm.advisor_netID==query_params['advisor_netID'])
+            forms = query.fetch(20)
+        elif form_type == 'second_reader': # form_type == 'second_reader':
+            query = SecondReaderForm.query()
+            if 'student_name' in query_params:
+                query.filter(SecondReaderForm.student_name==query_params['student_name'])
+            if 'student_netID' in query_params:
+                query.filter(SecondReaderForm.student_netID==query_params['student_netID'])
+            if 'advisor_name' in query_params:
+                query.filter(SecondReaderForm.advisor_name==query_params['advisor_name'])
+            if 'advisor_netID' in query_params:
+                query.filter(SecondReaderForm.advisor_netID==query_params['advisor_netID'])
+            forms = query.fetch(20)
+
+        query_params = {'forms': forms}
+        self.redirect('/forms/view_query?' + urllib.urlencode(query_params))
+
+class ViewQuery(webapp2.RequestHandler):
+
+    def get(self):
+        forms = self.request.get('forms')
+        template_values = {
+            'forms':forms,
+            'url': getLoginStatus(self.request.uri)[0], #url,
+            'url_linktext': getLoginStatus(self.request.uri)[1], #url_linktext,
+        }
+        template = JINJA_ENVIRONMENT.get_template('view_query.html')
+        self.response.write(template.render(template_values))
 
 
 application = webapp2.WSGIApplication([
@@ -270,4 +329,5 @@ application = webapp2.WSGIApplication([
     ('/forms/februaryform', FebruaryFormPage),
     ('/forms/view', FormView),
     ('/forms/query', FormQuery),
+    ('/forms/view_query', ViewQuery),
 ], debug=True)
