03fb9627bcef5785daff5c5b00045c0913966ae3
Adam Gallagher <asgpng@gmail.com>
2013-06-27 09:36:02 -0400

    small bug fixes

--- iw.py
+++ iw.py
@@ -36,76 +36,6 @@ config = {
     'secret_key': 'OUR_SECRET_KEY' # update later
 }
 
-# from http://blog.abahgat.com/2013/01/07/user-authentication-with-" \ 
-#             webapp2-on-google-app-engine/
-class BaseHandler(webapp2.RequestHandler):
-    @webapp2.cached_property
-    def auth(self):
-        """Shortcut to access the auth instance as a property."""
-        return auth.get_auth()
-        
-  @webapp2.cached_property
-  def user_info(self):
-      """Shortcut to access a subset of the user attributes that are stored
-    in the session.
- 
-    The list of attributes to store in the session is specified in
-      config['webapp2_extras.auth']['user_attributes'].
-    :returns
-      A dictionary with most user information
-    """
-    return self.auth.get_user_by_session()
-    
-  @webapp2.cached_property
-  def user(self):
-      """Shortcut to access the current logged in user.
- 
-    Unlike user_info, it fetches information from the persistence layer and
-    returns an instance of the underlying model.
- 
-    :returns
-      The instance of the user model associated to the logged in user.
-    """
-    u = self.user_info
-    return self.user_model.get_by_id(u['user_id']) if u else None
-    
-  @webapp2.cached_property
-  def user_model(self):
-      """Returns the implementation of the user model.
- 
-    It is consistent with config['webapp2_extras.auth']['user_model'], if set.
-    """   
-    return self.auth.store.user_model
-    
-  @webapp2.cached_property
-  def session(self):
-      """Shortcut to access the current session."""
-      return self.session_store.get_session(backend="datastore")
-      
-  def render_template(self, view_filename, params={}):
-      user = self.user_info
-      params['user'] = user
-      path = os.path.join(os.path.dirname(__file__), 'views', view_filename)
-      self.response.out.write(template.render(path, params))
-      
-  def display_message(self, message):
-      """Utility function to display a template with a simple message."""
-      params = {
-          'message': message
-      }
-    self.render_template('message.html', params)
-    
-  # this is needed for webapp2 sessions to work
-  def dispatch(self):
-      # Get a session store for this request.
-      self.session_store = sessions.get_store(request=self.request)
-      
-      try:
-          # Dispatch the request.
-          webapp2.RequestHandler.dispatch(self)
-      finally:
-          # Save all sessions.
-          self.session_store.save_sessions(self.response)
 
 
 class MainPage(webapp2.RequestHandler):
@@ -302,8 +232,8 @@ class FormDeleteConfirmation(webapp2.RequestHandler):
     def get(self):
         query_params = build_query_params(self)
         template_values = {
-            'student_netID':query_params['student_netID']
-            'form_type':query_params['form_type'],
+            'student_netID':query_params['student_netID'],
+            'form_type':query_params['form_type']
         }
         template = JINJA_ENVIRONMENT.get_template('form_delete_confirmation.html')
         self.response.write(template.render(template_values))  
