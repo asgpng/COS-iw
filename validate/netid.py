d1fc4434a148ae3a08283878e13fab22022a6742
Olivia <opb@princeton.edu>
2013-07-10 14:30:04 -0400

    deleted

--- validate/netid.py
+++
@@ -1,5 +0,0 @@
-def validate(netID):
-	 query_params = {'netID': sf.advisor_netID}
-     query = object_query(Faculty, query_params)
-     user_faculty = query.get()
-     return (user_faculty == NoneType)	
